483389f1bde0aaf8cdfdd764eadaa0e519027f01
mramirez92 <ramirez.92.monica@gmail.com>
2023-02-22 13:31:47 -0600

    don and monica model mods

--- thrive/models.py
+++ thrive/models.py
@@ -4,15 +4,15 @@ from django.urls import reverse
 
 
 class Thrive(models.Model):
-    name = models.CharField(max_length=256)
-    rating = models.IntegerField(default=0, blank=True)
-    reviewer = models.ForeignKey(
+    # name = models.CharField(max_length=256)
+    user = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
-    description = models.TextField(default="", null=True, blank=True)
+    task = models.TextField(default="", null=True, blank=True)
+    is_checked = models.BooleanField(default=0, blank=False)
 
     def __str__(self):
-        return self.name
+        return self.task
 
     def get_absolute_url(self):
         return reverse('thrive_detail', args=[str(self.id)])
